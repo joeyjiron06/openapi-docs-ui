0550100190f81236aacb62eec0027ad2b28e6946
Joey Jiron <joeyjiron06@gmail.com>
2018-12-08 16:25:19 -0800

    added styling to the table

--- .storybook/parameterTableStory.js
+++ .storybook/parameterTableStory.js
@@ -1,37 +1,70 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { StyleSheet, css } from 'aphrodite/no-important';
 import ParameterTable from '../src/components/parameterTable';
 
-storiesOf('ParameterTable', module).add('default', () => (
-  <ParameterTable
-    parameters={[
-      {
-        name: { title: 'email' },
-        type: {
-          subtitles: [{ title: 'email', color: 'default' }],
-          headers: [{ title: 'exactly', color: 'default' }],
-          titles: [{ title: 'string' }]
+storiesOf('ParameterTable', module).add('default', () => {
+  const styles = StyleSheet.create({
+    root: {
+      minWidth: 700,
+      margin: 10
+    }
+  });
+
+  return (
+    <ParameterTable
+      className={styles.root}
+      parameters={[
+        {
+          name: {
+            title: 'email',
+            subtitles: [
+              { title: 'required', color: 'red' },
+              {
+                title: 'deprecated',
+                color: 'yellow'
+              }
+            ]
+          },
+          type: {
+            subtitles: [{ title: 'email', color: 'default' }],
+            titles: [{ title: 'string' }, { title: 'integer' }]
+          },
+          description: 'This is a users email'
         },
-        description: 'This is a users email'
-      },
-      {
-        name: { title: 'User' },
-        type: {
-          titles: [
-            {
-              title: 'string'
-            },
-            {
-              title: 'User',
-              link: '/models/user'
-            },
-            {
-              title: 'Person',
-              link: '/models/person'
-            }
-          ]
+        {
+          name: { title: 'user' },
+          type: {
+            headers: [{ title: 'array of any of' }],
+            subtitles: [
+              { title: 'email' },
+              { title: 'user' },
+              { title: 'person' }
+            ],
+            titles: [
+              {
+                title: 'string'
+              },
+              {
+                title: 'User',
+                link: '/models/user'
+              },
+              {
+                title: 'Person',
+                link: '/models/person'
+              }
+            ]
+          },
+          description: `
+            **User Type**
+
+            Can be one of the following
+          `
+            .split('\n')
+            .map(line => line.trim())
+            .join('\n')
         }
-      }
-    ]}
-  />
-));
+      ]}
+    />
+  );
+});
--- src/components/parameterTable.js
+++ src/components/parameterTable.js
@@ -1,30 +1,46 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { ParameterType } from '../types';
 import { Parser, HtmlRenderer } from 'commonmark';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import { ParameterType } from '../types';
 
-const ParameterTable = ({ parameters }) => (
-  <table>
-    <thead>
+const colors = {
+  red: '#F25F5C',
+  yellow: '#F8E71C',
+  default: '#FFFFFF70',
+  green: '#B8E986'
+};
+
+const ParameterTable = ({ parameters, className }) => (
+  <table className={css(styles.root, className)}>
+    <thead className={css(styles.head)}>
       <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Description</th>
+        <th className={css(styles.tableData)}>Name</th>
+        <th className={css(styles.tableData)}>Type</th>
+        <th className={css(styles.tableData)}>Description</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody className={css(styles.body)}>
       {parameters.map(({ name, type, description }) => (
-        <tr key={name.title}>
-          <td>
+        <tr key={name.title} className={css(styles.tableRow)}>
+          <td className={css(styles.tableData)}>
             {renderParam({
-              ...name,
+              headers: name.headers,
+              subtitles: name.subtitles,
               titles: [{ title: name.title }]
             })}
           </td>
 
-          <td data-testid="parameter-type">{renderParam(type)}</td>
+          <td className={css(styles.tableData)} data-testid="parameter-type">
+            {renderParam({
+              headers: type.headers,
+              subtitles: type.subtitles,
+              titles: type.titles
+            })}
+          </td>
 
           <td
+            className={css(styles.tableData)}
             dangerouslySetInnerHTML={{
               __html: renderMarkdown(description)
             }}
@@ -36,8 +52,52 @@ const ParameterTable = ({ parameters }) => (
 );
 
 ParameterTable.propTypes = {
-  parameters: PropTypes.arrayOf(ParameterType).isRequired
+  parameters: PropTypes.arrayOf(ParameterType).isRequired,
+  className: PropTypes.string
 };
+
+const styles = StyleSheet.create({
+  root: {
+    color: 'white',
+    textAlign: 'left',
+    borderCollapse: 'collapse',
+    borderRadius: 6,
+    overflow: 'hidden'
+  },
+  head: {
+    backgroundColor: '#2A2A2A',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  body: {
+    backgroundColor: '#484848'
+  },
+  tableData: {
+    verticalAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    ':first-child': {
+      paddingLeft: 20
+    }
+  },
+  tableRow: {
+    borderBottomColor: '#565656',
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid'
+  },
+
+  link: {
+    color: 'white'
+  },
+  param: {},
+  paramMainTitles: {
+    fontFamily: `'Roboto Mono', 'monospace'`,
+    fontWeight: 500
+  },
+  paramTitles: {
+    fontSize: 11
+  }
+});
 
 const renderMarkdown = (text = '') =>
   new HtmlRenderer().render(
@@ -48,33 +108,37 @@ const renderMarkdown = (text = '') =>
   );
 
 const renderParam = ({ headers, subtitles, titles }) => (
-  <Fragment>
-    <div>
-      {(headers || []).map(({ title }) => (
-        <div key={title}>{title}</div>
+  <div className={css(styles.param)}>
+    {renderTitles(headers)}
+
+    <div className={css(styles.paramMainTitles)}>
+      {titles.map(({ title, link }, index) => (
+        <span key={title}>
+          {link ? (
+            <a href={link} className={css(styles.link)}>
+              {title}
+            </a>
+          ) : (
+            title
+          )}
+
+          {index !== titles.length - 1 && <span> | </span>}
+        </span>
       ))}
     </div>
 
-    {titles.map(({ title, link }, index) => (
-      <div key={title}>
-        {link ? (
-          <a key={title} href={link}>
-            {title}
-          </a>
-        ) : (
-          title
-        )}
+    {renderTitles(subtitles)}
+  </div>
+);
 
-        {index !== titles.length - 1 && <span> or </span>}
+const renderTitles = (titles = []) => (
+  <div className={css(styles.paramTitles)}>
+    {titles.map(({ title, color = 'default' }) => (
+      <div key={title} style={{ color: colors[color] }}>
+        {title}
       </div>
     ))}
-
-    <div>
-      {(subtitles || []).map(({ title }) => (
-        <div key={title}>{title}</div>
-      ))}
-    </div>
-  </Fragment>
+  </div>
 );
 
 export default ParameterTable;
